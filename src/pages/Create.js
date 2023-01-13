bf1567e9d219dc3a62798b79db4fb5fa2f3a44fc
ViDA_TG <victorademiju.t@gmail.com>
2023-01-13 04:24:07 +0100

    Lesson 5

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -19,9 +19,9 @@ const Create = () => {
     }
 
     const { data, error } = await supabase
-      .from('recipes')
+      .from('smoothies')
       .insert([{ title, method, rating }])
-
+      .select()
     if (error) {
       console.log(error)
       setFormError('Please fill in all the fields correctly.')
