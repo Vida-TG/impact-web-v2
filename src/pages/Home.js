1d86a5dff052d54f472fada6c6dfae1a329c8eb1
ViDA_TG <victorademiju.t@gmail.com>
2023-01-13 16:04:36 +0100

    Order

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,13 @@ import SmoothieCard from "../components/SmoothieCard"
 const Home = () => {
   const [ fetchError, setFetchError ] = useState(null)
   const [ smoothies, setSmoothies ] = useState(null)
+  const [ orderBy, setOrderBy ] = useState('created_at')
 
   const fetchSmoothies = async () => {
     const { data, error } = await supabase
       .from('smoothies')
       .select()
+      .order(orderBy, {ascending: false})
 
       if (error) {
         setFetchError('Could not fetch')
@@ -29,13 +31,20 @@ const Home = () => {
 
   useEffect(() => {
     fetchSmoothies()
-  }, [])
-  
+  }, [orderBy])
+
   return (
     <div className="page home">
       { fetchError && (<p>{fetchError}</p>)}
       { smoothies && (
         <div className="smoothies">
+          <div className="order-by">
+            <p>Order by:</p>
+            <button onClick={() => setOrderBy('created_at')}>Time Created</button>
+            <button onClick={() => setOrderBy('title')}>Title</button>
+            <button onClick={() => setOrderBy('rating')}>Rating</button>
+            {orderBy}
+          </div>
           <div className="smoothie-grid">
             { smoothies.map(smoothie => (
               <SmoothieCard key={smoothie.id} smoothie={smoothie} onDelete={handleDelete}/>
